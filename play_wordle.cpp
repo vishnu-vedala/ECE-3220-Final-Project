7c7163796037a1ebfae7619b2ca517bd8979eade
Vishnu Vedala <vishnuvedala@Vishnus-MacBook-Pro.local>
2022-05-12 22:02:24 -0500

    Add small changes to getAnswer function

--- play_wordle.cpp
+++ play_wordle.cpp
@@ -5,19 +5,27 @@ Play_Wordle::Play_Wordle(){
 }
 
 std::string Play_Wordle::getAnswer(){
+    GameMode* game = new GameMode();
     std::string answer;
     std::cout << "Input desired answer as a 5 (lowercase) letter word or a 3 digit day number (Ex: 001 for Day 1 Wordle solution): ";
     std::cin >> answer;
-    for(long unsigned i = 0; i < Singleton::getInstance()->getlists()->word_List.size(); i++){
-        std::string temp = Singleton::getInstance()->getlists()->word_List[i];
-        if(answer == temp){
-            std::cout << "Word accepted...Beginning Wordle..." << std::endl;
-            break;
+    if(answer.size() == 5){
+        for(long unsigned i = 0; i < Singleton::getInstance()->getlists()->word_List.size(); i++){
+            std::string temp = Singleton::getInstance()->getlists()->word_List[i];
+            if(answer == temp){
+                std::cout << "Word accepted...Beginning Wordle..." << std::endl;
+                
+                break;
+            }
+            if(i + 1 == Singleton::getInstance()->getlists()->word_List.size()){
+                std::cout << "Word not found in the Wordle word list. Please try again." << std::endl;
+                return; 
+            }
         }
-        if(i + 1 == Singleton::getInstance()->getlists()->word_List.size()){
-            std::cout << "Word not found in the Wordle word list. Please try again." << std::endl;
-            return; 
-        }
+    }
+    else if(answer.size() != 5)
+        std::cout << "Word is not 5 letters long, please input a new word." << std::endl;
+        getAnswer();
     }
 }
 
